2c866c06dc570256ef22e06886e7893b372e0e22
wz <wenzhe1423@gmail.com>
2026-01-19 13:45:00 +0800

    Feat: Landing Input Page

--- nomnom/components/Navbar.jsx
+++ nomnom/components/Navbar.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="w-full p-4 px-8 text-white flex flex-row justify-between items-center font-sans font-semibold bg-zinc-900">
+    <div className="w-full p-4 px-8 text-white flex flex-row justify-between items-center font-sans font-semibold bg-zinc-900 sticky top-0 z-50">
       <Link href="/">
         <p className="text-2xl">NomNom</p>
       </Link>
---
+++ nomnom/src/app/page.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+export default function Home() {
+  function handleFileUpload(e) {
+    let uploadedFile = e.target.files[0];
+    // Send File to Backend
+  }
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black font-sans">
+      <main className="flex flex-col items-center gap-6 bg-white dark:bg-black p-8 rounded-2xl">
+        <div className="flex flex-col items-center gap-2 text-center">
+          <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            Upload CSV File
+          </h1>
+          <p className="text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Upload your sales CSV and get instant insights to monitor food
+            ingredient usage.
+          </p>
+        </div>
+        <input
+          type="file"
+          id="file-upload"
+          className="hidden"
+          // accept=".csv"
+          onChange={handleFileUpload}
+        />
+
+        <label
+          htmlFor="file-upload"
+          className="rounded-2xl text-black p-3.5 bg-white mt-4 shadow-md hover:cursor-pointer"
+        >
+          Upload CSV Here
+        </label>
+      </main>
+    </div>
+  );
+}
