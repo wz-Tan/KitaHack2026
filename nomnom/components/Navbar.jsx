095908a753ac5a7db65b7dca1615bed3e90a5b3e
wz <wenzhe1423@gmail.com>
2026-01-19 09:03:12 +0800

    Chore: Set Up Front-End Structure

---
+++ nomnom/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <div className="w-full p-4 px-8 text-white flex flex-row justify-between items-center font-sans font-semibold bg-zinc-900">
+      <Link href="/">
+        <p className="text-2xl">NomNom</p>
+      </Link>
+      <div className="flex flex-row gap-10">
+        <Link href="/">
+          <p>Home</p>
+        </Link>
+        <Link href="/dashboard">
+          <p>Dashboard</p>
+        </Link>
+        <Link href="/insights">
+          <p>Insights</p>
+        </Link>
+      </div>
+      <Link href="/signIn">
+        <p>Sign In</p>
+      </Link>
+    </div>
+  );
+}
--- nomnom/src/app/page.js
+++ nomnom/src/app/page.js
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -27,7 +25,6 @@ export default function Home() {
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-         
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
